c14b3f73916ea2cc340924ff2a5e7d4f8c59fd3b
Caio Moreira <caiomcarvalho@uniplaclages.edu.br>
2024-08-27 23:54:36 -0300

    Added experience translations e simplified listings

--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
 document.getElementById('searchButton').addEventListener('click', function() {
+
     // Obter os valores digitados
     const jobTitles = document.getElementById('jobTitle').value.split(',').map(title => title.trim()).filter(title => title !== '');
-    const jobLevel = document.getElementById('experienceLevel').value;
+    var jobLevel = document.getElementById('experienceLevel').value;
     const jobIgnore = document.getElementById('jobIgnore').value.split(',').map(ignore => ignore.trim()).filter(ignore => ignore !== '');
+    // Obter Checkboxes
     const remoteOnly = document.getElementById('remote').checked;
+    const candidaturaSimplificada = document.getElementById('simplificada').checked;
+    //geoId do Brasil
+    const isBrasil = 106057199
+
+    // Adicionar geoId para buscar vagas  por  país
+    var geoId = document.getElementById('countryId').value;    
 
     // Validação
     if (jobTitles.length === 0 || !jobLevel) {
@@ -11,12 +19,38 @@ document.getElementById('searchButton').addEventListener('click', function() {
         return; // Não prosseguir com a ação se a validação falhar
     }
 
-    // Construir a string de pesquisa booleana
-    const booleanSearch = jobTitles.map(title => `"${title}" AND ${jobLevel}`).join(' OR ');
+    //Configurando para Inglês nível do cargo caso seja fora do Brasil a vaga
+    if (geoId !== isBrasil) 
+    { 
+        if (jobLevel === 'estágio') {
+            jobLevel = 'intern';
+        }
+    
+        if (jobLevel === 'pleno') {
+            jobLevel = '';
+        }
+    }
+
+    //Define a query padrão
+    var booleanSearch = jobTitles.map(title => `"${title}" AND ${jobLevel}`).join(' OR ');
+
+    //Modificar o query para pleno gringo pois lá não se falan "pleno" apenas "Developer" por exemplo
+    if(geoId !== isBrasil && jobLevel === '') //jobLevel é pleno
+    {
+        booleanSearch = jobTitles.map(title => `"${title}"`).join(' OR ');
+    }
+    
+    //Define variável searchQuery
     let searchQuery = `${booleanSearch}`;
 
-    if (remoteOnly) {
+    //Adiciona Remoto na query
+    if (remoteOnly === true && geoId !== isBrasil) 
+    {
         searchQuery += ' AND remote';
+    } 
+    else if (remoteOnly) 
+    {
+        searchQuery += ' AND remoto';
     }
 
     // Adicionar 'AND NOT' se houver valores no campo jobIgnore
@@ -25,11 +59,16 @@ document.getElementById('searchButton').addEventListener('click', function() {
         searchQuery += ` AND NOT (${ignoreSearch})`;
     }
 
+    // criar url
 
-    // Adicionar geoId para buscar vagas  por  país
-    var geoId = document.getElementById('countryId').value;    
+    let url = `https://www.linkedin.com/jobs/search/?keywords=${encodeURIComponent(searchQuery)}&geoId=${encodeURIComponent(geoId)}`
 
-    // Abrir uma nova aba com a busca no LinkedIn
-    const url = `https://www.linkedin.com/jobs/search/?keywords=${encodeURIComponent(searchQuery)}&geoId=${encodeURIComponent(geoId)}`;
+    //adicionar candidatura simplificada na url caso marcado a checkbox
+    if(candidaturaSimplificada)
+    {
+        url += '&f_AL=true';
+    }
+
+    //Criar aba
     chrome.tabs.create({ url: url });
 });
